7e2efe3489f0b143861a9de5da769d9d2630c7bb
jackvadderr <jacjacckck@gmail.com>
2025-04-05 05:46:16 -0400

    feat: update event list preview

--- src/app/dashboard/events/page.tsx
+++ src/app/dashboard/events/page.tsx
@@ -1,11 +1,13 @@
 import EventListPage from '@/components/dashboard/events/EventListPage';
 import { getEvents } from '@/actions/list-all-events-action';
 import { createEventAction } from '@/actions/create-event-action';
+import { deleteEventsAction } from '@/actions/delete-event-action';
+import { updateEventAction } from '@/actions/update-event-action';
 
 export default async function DashboardEventsPage() {
   try {
     const events = await getEvents();
-    return <EventListPage initialEvents={events} createEventAction={createEventAction} />;
+    return <EventListPage initialEvents={events} createEventAction={createEventAction} updateEventAction={updateEventAction} deleteEventsAction={deleteEventsAction}/>;
   } catch (err) {
     console.error('Error:', err);
     return <p className="text-red-500">Falha ao carregar eventos. Tente novamente mais tarde.</p>;
